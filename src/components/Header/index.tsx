d454e5c40d5cb9d5c20993e868ecd07b63bdaa31
Jairo Doni <jairo.doni97@gmail.com>
2021-04-02 16:39:57 -0300

    next configurado e header pronto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
+import { useRouter } from 'next/router';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  const router = useRouter();
+
+  function handleGoHome() {
+    router.push(`/`);
+  }
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo" onClick={handleGoHome} />
+      </div>
+    </header>
+  );
 }
